93ec903cc96a89b17ae2b3cb4c868562218e616f
Andrew Kiellor <akiellor@gmail.com>
2015-07-14 08:28:14 -0700

    Fixed cleanup code for selenium launcher.

--- calculator_spec.js
+++ calculator_spec.js
@@ -1,5 +1,20 @@
 describe('calculator', function(){
-  it('should add', function(){
-    expect(1 + 1).toBe(2);
-  });
+  for (var i = 0; i < 500; i++) {
+    it('should add', function(done){
+      runs(function() {
+        flag = false;
+        value = 0;
+
+        setTimeout(function() {
+          value++;
+          //keep returning false…
+          flag = true;
+        }, 50);
+      }); 
+
+      waitsFor(function() {
+        return flag;
+      }, "The Value should be incremented", 5000); 
+    });
+  }
 });
